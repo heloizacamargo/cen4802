00ba7c0343ea1459c60b0ee9d74f3f59162b8183
Heloiza Camargo <heloizacamargo@outlook.com>
2024-01-20 14:43:34 -0500

    doc comments added

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,10 +1,22 @@
+/**
+ * This class is used to calculate the nth term in the Fibonacci sequence.
+ * 
+ * @author Heloiza Camargo
+ */
 public class Fibonacci {
+    
     public static void main(String args[]){
         int n = 10;
         int result = fibonacci(n);
         System.out.println("The " + n + "th term in the Fibonacci sequence is: " + result);
     }
 
+    /**
+     * Calculates the nth term in the Fibonacci sequence.
+     *
+     * @param n The position of the term to be calculated (non-negative integer).
+     * @return The nth term in the Fibonacci sequence.
+     */
     public static int fibonacci(int n) {
         if (n <= 1) {
             return n;
